084ac97b48ae03b1de5cbd1422db87bf7332aff3
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-05-12 10:52:41 +0300

    Добавляет пагинацию

--- src/js/test.js
+++ src/js/test.js
@@ -1,15 +1,22 @@
 import EventsApiService from './api/EventsApiService';
 import notificationError from './notification-func';
+import Pagination from 'tui-pagination';
+import options from './pagination-tui';
 
 // Создаём экземпляр класса
 const eventsApiService = new EventsApiService();
+
+
 
 // Список случайных событий
 // eventsApiService.fetchRandomEvents().then(console.log).catch(console.log);
 
 // Список событий по ключевому слову
 eventsApiService.query = 'music';
-eventsApiService.fetchEventsByKeyWord().then(console.log).catch(console.log);
+eventsApiService.fetchEventsByKeyWord().then(result => {
+    const pagination = new Pagination('pagination', options);
+    pagination._options.totalItems = eventsApiService.totalElements;
+}).catch(console.log);
 
 // Список событий по стране
 // eventsApiService.fetchEventsByCoutry('US').then(console.log).catch(console.log);
