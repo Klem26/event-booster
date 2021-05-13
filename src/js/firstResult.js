6c6d669654d26a2016b1668cc09312f427353842
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-05-13 23:09:44 +0300

    bugfix/убрала лишнюю переменную

--- src/js/firstResult.js
+++ src/js/firstResult.js
@@ -2,6 +2,4 @@ import pageRender from './page-render';
 import EventsApiService from './api/EventsApiService';
 
 const firstEventsList = new EventsApiService();
-const renderFirstPage = firstEventsList
-  .fetchRandomEvents()
-  .then(data => pageRender(data));
+firstEventsList.fetchRandomEvents().then(data => pageRender(data));
