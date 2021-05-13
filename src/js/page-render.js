075aa690ac91b07a7b94da68c32578133b5300dd
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-05-13 17:21:39 +0300

    Revert "basic render done"

--- src/js/page-render.js
+++
@@ -1,8 +0,0 @@
-import eventsTemplate from './templates/event-list.hbs';
-import refs from './refs';
-
-export default function pageRender(data) {
-    const markup = eventsTemplate(data);
-    refs.containerResult.innerHTML = '';
-    refs.containerResult.insertAdjacentHTML('beforeend', markup);
-}
