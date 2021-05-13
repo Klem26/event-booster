84ec30c8a869138760d507982a76678f06b0db74
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-05-13 16:47:05 +0300

    fn fix

--- src/js/page-render.js
+++ src/js/page-render.js
@@ -2,7 +2,7 @@ import eventsTemplate from './templates/event-list.hbs';
 import refs from './refs';
 
 export default function pageRender(data) {
-    const markup = eventsTemplate(...data);
+    const markup = eventsTemplate(data);
     refs.containerResult.innerHTML = '';
     refs.containerResult.insertAdjacentHTML('beforeend', markup);
 }
