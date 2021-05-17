f8fdabc540f538dc07bf38a970f6ff368c738cdc
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-05-17 15:17:58 +0300

    sign up

--- src/js/firebase/auth-modal.js
+++ src/js/firebase/auth-modal.js
@@ -1,12 +1,13 @@
 import refs from '../refs';
 import Events from './Events';
+import { signUp } from './firebase-auth';
 
 const signInBtn = refs.authBtn;
 
 signInBtn.addEventListener('click', openModal);
 
 function openModal() {
-  createModal('Authorization', getAuthForm());
+  createModal('Sign up', getAuthForm());
   document
     .getElementById('auth-form')
     .addEventListener('submit', authFormHandler, { once: true });
@@ -18,20 +19,20 @@ export function authFormHandler(event) {
   const email = event.target.querySelector('#email').value;
   const password = event.target.querySelector('#password').value;
 
-   return [email, password];
+  signUp(email, password);
 }
 
 function createModal(title, content) {
   const modal = document.createElement('div');
-    modal.classList.add('auth-modal');
-    
-    const html = `<h1>${title}</h1><div class='modal-content'>${content}</div>`;
-    modal.innerHTML = html;
+  modal.classList.add('auth-modal');
+
+  const html = `<h1>${title}</h1><div class='modal-content'>${content}</div>`;
+  modal.innerHTML = html;
 
   mui.overlay('on', modal);
 }
 
- function getAuthForm() {
+function getAuthForm() {
   return `<form class="mui-form" id="auth-form">
       <div class="mui-textfield mui-textfield--float-label">
         <input type="email" id="email" required>
